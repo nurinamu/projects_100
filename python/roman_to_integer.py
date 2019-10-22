bdaa146bf755278d53a8e2c16d8796ab75fd34ba
nurinamu <nurinamu@gmail.com>
2019-10-23 00:39:25 +0900

    reduce few memories

--- python/roman_to_integer.py
+++ python/roman_to_integer.py
@@ -1,11 +1,18 @@
 class Solution:
-    numStr = "MDCLXVI"
-    numStrVal = [1000, 500, 100, 50, 10, 5, 1]
+    dic = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1
+    }
     def romanToInt(self, s: str) -> int:        
         sum = 0
         prevVal = 0
-        for i in range(len(s)-1 , -1, -1) :
-            curVal = Solution.numStrVal[Solution.numStr.index(s[i])]
+        for i in range(len(s) - 1 , -1, -1) :
+            curVal = Solution.dic[s[i]]
             if prevVal <= curVal :
                 sum += curVal
             else :
@@ -16,7 +23,7 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    print("result : {}".format(solution.romanToInt('III')))
+    print("result : {}".format(solution.romanToInt('LVIII')))
     print("result : {}".format(solution.romanToInt('IV')))
     print("result : {}".format(solution.romanToInt('IX')))
     print("result : {}".format(solution.romanToInt('MCMXCIV')))
