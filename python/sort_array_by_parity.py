a6ffdc77172d0e6421a20a318371df68ab2ba8af
nurinamu <nurinamu@gmail.com>
2019-10-31 22:41:25 +0900

    update for time performance

--- python/sort_array_by_parity.py
+++ python/sort_array_by_parity.py
@@ -2,28 +2,15 @@ from typing import List
 
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        if len(A) == 0 :
-            return []
-        rightIdx = len(A) - 1
-        leftIdx = 0
-        while leftIdx < rightIdx :
-            while leftIdx < len(A) :
-                if A[leftIdx] % 2 == 1 :
-                    break
-                else :             
-                    leftIdx += 1
-            while rightIdx >= 0 :
-                if A[rightIdx] %2 == 0 :
-                    break
-                else :
-                    rightIdx -= 1
-            if leftIdx < rightIdx :        
-                tmp = A[leftIdx]
-                A[leftIdx] = A[rightIdx]
-                A[rightIdx] = tmp
-                leftIdx += 1
-                rightIdx -= 1
-        return A
+        odd = []
+        even = []
+        for num in A :
+            if num % 2 == 0 :
+                even.append(num)
+            else :
+                odd.append(num)
+        even.extend(odd)
+        return even
 
 if __name__ == "__main__":
     solution = Solution()
