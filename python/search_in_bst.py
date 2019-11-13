400dcce6973d99c81c9f838b24ebeaed90bfc18b
nurinamu <nurinamu@gmail.com>
2019-11-13 23:09:34 +0900

    Day 23 search in bst

---
+++ python/search_in_bst.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+    def __str__(self):
+        result = "({})".format(self.val)
+        if self.left != None :
+            result += "-L[{}]".format(self.left)
+        if self.right != None :
+            result += "-R[{}]".format(self.right)
+        return result
+
+class Solution:
+    
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        print("root.val[{}] : {}".format(root.val, val))
+        if root.val == val :
+            return root
+        if root.left != None :
+            result = self.searchBST(root.left, val)
+            if result != None :
+                return result
+        if root.right != None :
+            result = self.searchBST(root.right, val)
+            if result != None :
+                return result
+        return None
+
+if __name__ == "__main__":
+    sol = Solution()
+    root = TreeNode(18)
+    
+    root.left = TreeNode(2)
+    root.right = TreeNode(22)
+    root.right.right = TreeNode(63)
+    root.right.right.right = TreeNode(84)
+
+    # [18,2,22,null,null,null,63,null,84,null,null]
+
+    print(sol.searchBST(root, 63))
